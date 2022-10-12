6c058b21493a455417a9167d76e4f371aac5fee8
CRazorback <chesterrazorback@gmail.com>
2022-10-12 12:51:13 +0800

    update heads.py

--- models/heads.py
+++ models/heads.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from .modules import Activation
+from .modules import Flatten, Activation
 
 
 class SegmentationHead(nn.Sequential):
@@ -13,12 +13,13 @@ class SegmentationHead(nn.Sequential):
 
 class ClassificationHead(nn.Sequential):
 
-    def __init__(self, in_channels, classes, pooling="avg", dropout=0.2, activation=None):
+    def __init__(self, in_channels, classes=2, pooling="avg", dropout=0.2, activation=None):
         if pooling not in ("max", "avg"):
             raise ValueError("Pooling should be one of ('max', 'avg'), got {}.".format(pooling))
         pool = nn.AdaptiveAvgPool2d(1) if pooling == 'avg' else nn.AdaptiveMaxPool2d(1)
-        flatten = nn.Flatten()
-        dropout = nn.Dropout(p=dropout, inplace=True) if dropout else nn.Identity()
-        linear = nn.Linear(in_channels, classes, bias=True)
-        activation = Activation(activation)
-        super().__init__(pool, flatten, dropout, linear, activation)
+        flatten = Flatten()
+        # dropout = nn.Dropout(p=dropout, inplace=True) if dropout else nn.Identity()
+        # linear = nn.Linear(in_channels, classes, bias=True)
+        # activation = Activation(activation)
+        # super().__init__(pool, flatten, dropout, linear, activation)
+        super().__init__(pool, flatten)
